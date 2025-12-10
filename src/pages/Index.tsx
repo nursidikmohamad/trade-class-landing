d6791df5655567571d1f2ed94389add23b120789
nursidikimohamad2-jpg <nursidikimohamad2@gmail.com>
2025-12-10 23:30:17 +0800

    update

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,7 +1,7 @@
 import Hero from "@/components/Hero";
 import VideoSection from "@/components/VideoSection";
 import Features from "@/components/Features";
-import RegistrationForm from "@/components/RegistrationForm";
+// import RegistrationForm from "@/components/RegistrationForm";
 import Footer from "@/components/Footer";
 
 const Index = () => {
@@ -10,7 +10,7 @@ const Index = () => {
       <Hero />
       <VideoSection />
       <Features />
-      <RegistrationForm />
+      {/* <RegistrationForm /> */}
       <Footer />
     </main>
   );
